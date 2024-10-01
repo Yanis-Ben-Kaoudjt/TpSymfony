5fe3240ca40f51af7485c66ff449c0553dceec2c
Yanis Ben Kaoudjt <yanis.benkaoudjt@edu.univ-paris13.fr>
2024-10-01 18:51:49 +0200

    Entities creation

---
+++ streemi/src/Enum/UserAccountStatusEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum UserAccountStatusEnum: string
+{
+    case ACTIVE = 'active';
+    case PENDING = 'pending';
+    case BLOCKED = 'blocked';
+    case BANNED = 'banned';
+    case DELETED = 'deleted';
+}
